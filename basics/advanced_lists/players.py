856b056e28f3cddd5c26e5b7110b02c7b7cb4cf5
MaxCherya <maxchergik@gmail.com>
2026-03-08 19:41:50 +0000

    advanced lists

---
+++ basics/advanced_lists/players.py
@@ -0,0 +1,15 @@
+players = [
+    "Lionel Messi",
+    "Cristiano Ronaldo",
+    "Neymar Jr.",
+    "Kylian Mbappé",
+    "Kevin De Bruyne",
+    "Virgil van Dijk",
+    "Mohamed Salah",]
+
+print(players[:2])  # First two players
+print(players[2:5])  # Players from index 2 to 4
+print(players[-3:])  # Last three players
+
+for player in players[:3]:
+    print(player)  # Print the first three players
